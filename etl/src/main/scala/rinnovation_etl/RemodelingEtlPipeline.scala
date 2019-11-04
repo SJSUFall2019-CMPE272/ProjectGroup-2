993781d623bbc28a0541e9b2c8a838cc4274868d
Andrew Selvia <andrew@selvia.me>
2019-11-04 02:34:30 -0800

    Add massaging logic so that the CSVs are usable

--- etl/src/main/scala/rinnovation_etl/RemodelingEtlPipeline.scala
+++ etl/src/main/scala/rinnovation_etl/RemodelingEtlPipeline.scala
@@ -18,6 +18,9 @@ import scala.sys.process._
 object RemodelingEtlPipeline extends App {
   private val browser = JsoupBrowser()
   private val regionsDirectoryName = "regions"
+  private val citiesWithoutPdfsIn2019 = List(
+    "daytonabeachfl",
+    "myrtlebeachsc")
   new File(getClass.getResource(s"/$regionsDirectoryName").getPath)
     .listFiles
     .toSeq
@@ -32,7 +35,8 @@ object RemodelingEtlPipeline extends App {
         .toString
         .replace("-", "")
         .replace("/", ""))
-    .filter(_ == "sandiegoca") // TODO: remove this when you move to production; only for testing
+    .filterNot(citiesWithoutPdfsIn2019.contains) // Some cities do not have accessible PDFs, they result in errors
+//    .filter(_ == "sandiegoca") // TODO: remove this when you move to production; only for testing
     .foreach(cityName => {
       val byteArrayOutputStream = new ByteArrayOutputStream
       writePdfFileContentsToByteArrayOutputStream(cityName, byteArrayOutputStream)
@@ -47,7 +51,40 @@ object RemodelingEtlPipeline extends App {
     val page = getPage(byteArray, pageNumber).getArea(125, 45, 600, 745)
     val bea = new BasicExtractionAlgorithm
     val table = bea.extract(page).get(0)
-    new CSVWriter().write(new FileWriter(s"$cityName.csv"), table)
+    val file = new File(s"/tmp/rinnovation/$cityName.csv")
+    new CSVWriter().write(new FileWriter(file), table)
+    massageCsv(cityName, file)
+  }
+
+  def massageCsv(cityName: String, file: File): Unit = {
+    import com.github.tototoshi.csv.{CSVParser, CSVWriter}
+    val source = Source.fromFile(file)
+    val values = source
+      .getLines
+      .filter(_.endsWith("%"))
+      .map(
+        CSVParser
+          .parse(_, '\\', ',', '"')
+          .getOrElse(List[String]())
+          .map(
+            _
+              .replace(",", "")
+              .replace("\"", "")
+              .replace("%", "")
+              .replace("$", ""))
+          .flatMap(line =>
+            if (line.contains(" ") && line.toLowerCase == line) {
+              line.split(" ").map(_.trim)
+            } else {
+              List(line)
+            }
+          )
+          .filter(_.nonEmpty))
+      .toSeq
+      .map(2019 :: cityName :: _)
+    CSVWriter
+      .open(file)
+      .writeAll(values)
   }
 
   @throws[IOException]
